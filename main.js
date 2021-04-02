47b044f6bb0abc1b35f268108d08db7715d7b78e
harshaathnere <harshaathnere@gmail.com>
2021-04-02 15:36:08 +0530

    updation

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ document.addEventListener('DOMContentLoaded',() =>{
     const result = document.querySelector('#result');
     const displayCurrentPlayer = document.querySelector('#current-player');
     let currentPlayer =1;
+ const winningArray =[]
+
+    function checkBoard(){
+    for (let y =0; winningArray.length; y++){
+        
+    }
+    }
     for (let i=0; i< square.length; i++){
-        square[i].onclick =() =>{
-            alert('You have clicked' +i);
+              square[i].onclick =() =>{
+
+            if(square[i +7].classList.contains('taken')){
+                  if(currentPlayer ==1){
+                    square[i].classList.add('taken')
+                    square[i].classList.add('player-one')
+                    currentPlayer =2;
+                  displayCurrentPlayer.innerHTML = currentPlayer
+                   }
+                  else if(currentPlayer ==2){
+                    square[i].classList.add('taken')
+                    square[i].classList.add('player-Two')
+                    currentPlayer =1;
+                   displayCurrentPlayer.innerHTML = currentPlayer
+                   }
+                
+            } else
+            alert("Can not go here")
+
+            checkBoard()
+
+           
         }
     }
 })
